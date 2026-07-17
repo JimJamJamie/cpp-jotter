6d39caf5cd8bc15093646cfbe569c65ebb6d9d93
James Rennie <21192547+JimJamJamie@users.noreply.github.com>
2026-07-17 20:54:26 +0100

    added clean flag to build, added exception handler and helper classes, added docs

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,9 +1,57 @@
-#include <fmt/core.h>
+#include <string>
 #include <iostream>
+#include <fmt/core.h>
 
-using namespace std;
+int ERROR_LEVEL = 0;
+
+class Printer {
+public:
+
+  static void print(std::string printSpool = "\n") {
+    fmt::print(printSpool);
+  }
+};
+
+class Screen: public Printer {
+public:
+
+  static void clear() {
+    print("\033[H\033[J");
+  }
+};
+
+class ErrorHandler {
+public:
+
+  static void setErrorLevel(int errorLevel) {
+    ERROR_LEVEL = errorLevel;
+  }
+};
+
+class Application {
+public:
+
+  Printer printer;
+  Screen screen;
+
+  void run() {
+    screen.clear();
+    printer.print("Hello World!");
+    printer.print();
+  }
+};
 
 int main() {
-  fmt::print("Hello World!\n");
-  return 0;
+
+  try {
+    Application app;
+    app.run();
+  }
+  catch( ... ) {
+    ErrorHandler::setErrorLevel(1);
+  }
+
+  Printer::print("Press any key to exit...");
+  std::cin.get();
+  return ERROR_LEVEL;
 }
