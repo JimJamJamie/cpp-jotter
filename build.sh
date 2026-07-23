a61963e9947f18553b563b748907560e3e8fd677
James Rennie <21192547+JimJamJamie@users.noreply.github.com>
2026-07-23 02:53:48 +0100

    added g++ command line build fix to build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,10 @@ cmake \
 -G Ninja \
 -Dclean=$clean \
 -DCMAKE_BUILD_TYPE="MinSizeRel" \
--DCMAKE_CXX_FLAGS="-Wall -Wextra -pedantic -flto"
+-DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" # g++ needs this parameter or the build fails.
+
+# Debug flags for gcc and clang
+# -DCMAKE_CXX_FLAGS="-Wall -Wextra -pedantic -flto"
 
 nproc=$(($(nproc)/2+2))
 ninja -C build -j$nproc
